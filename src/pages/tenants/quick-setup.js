256cde2b82da9be814bae159c645b399ac36563e
Joel Amoako <joelkofiamoako@gmail.com>
2024-02-03 12:53:16 +0000

    Quick Auto Commit

--- src/pages/tenants/quick-setup.js
+++ src/pages/tenants/quick-setup.js
@@ -1,23 +1,15 @@
 // ** MUI Imports
-import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
-import CardHeader from '@mui/material/CardHeader'
-import CardContent from '@mui/material/CardContent'
+import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+import StepperCustomHorizontal from 'src/ui/tenant/quick-setup/StepperCustomHorizontal'
 
 const TenantsQuickSetup = () => {
   return (
-    <Grid container spacing={6}>
+    <DatePickerWrapper>
       <Grid item xs={12}>
-        <Card>
-          <CardHeader title='Tenant Setup👯'></CardHeader>
-          <CardContent>
-            <Typography sx={{ mb: 2 }}>Setup your tenants.</Typography>
-            <Typography></Typography>
-          </CardContent>
-        </Card>
+        <StepperCustomHorizontal />
       </Grid>
-    </Grid>
+    </DatePickerWrapper>
   )
 }
 
