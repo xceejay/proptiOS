344cfe631310716159abb0feeeafe17368745578
Joel Amoako <joelkofiamoako@gmail.com>
2024-02-07 13:07:13 +0000

    Quick Auto Commit

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -66,20 +66,20 @@ const navigation = () => {
       title: 'Reports',
       path: '/reports',
       icon: 'tabler:receipt'
-    }
+    },
 
     // {
     //   title: 'Second Page',
     //   path: '/second-page',
     //   icon: 'tabler:mail'
     // },
-    // {
-    //   path: '/acl',
-    //   action: 'read',
-    //   subject: 'acl-page',
-    //   title: 'Access Control',
-    //   icon: 'tabler:shield'
-    // }
+    {
+      path: '/acl',
+      action: 'read',
+      subject: 'acl-page',
+      title: 'Access Control',
+      icon: 'tabler:shield'
+    }
   ]
 }
 
