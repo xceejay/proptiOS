3e7adbd9cdd8ccdee34c5dda12f1b5257ba9b529
Joel Amoako <joelkofiamoako@gmail.com>
2024-03-22 21:47:43 +0000

    Quick Auto Commit

--- src/pages/accounting/index.js
+++ src/pages/accounting/index.js
@@ -12,74 +12,72 @@ import { Box } from '@mui/system'
 
 const Accounting = () => {
   return (
-    <ApexChartWrapper>
-      <Grid container={12} xs={12} spacing={6}>
-        <Grid item lg={12}>
-          {/* <Grid item xs={12} md={8} lg={12}>
+    <Grid container={12} xs={12} spacing={6}>
+      <Grid item lg={12}>
+        {/* <Grid item xs={12} md={8} lg={12}>
           <EcommerceTransactionsHorizontal />
         </Grid> */}
-          <Grid container spacing={6}>
-            <Grid item xs={12} md={6} lg={8}>
-              <CrmEarningReportsWithTabs />
-            </Grid>
+        <Grid container spacing={6}>
+          <Grid item xs={12} md={6} lg={8}>
+            <CrmEarningReportsWithTabs />
+          </Grid>
 
-            <Grid item xs={12} lg={4}>
-              <Grid container spacing={6}>
-                <Grid item xs={6} md={3} lg={6}>
-                  <CardStatsVertical
-                    stats='1.28k'
-                    chipText='-12.2%'
-                    chipColor='default'
-                    avatarColor='info'
-                    title='Net Income'
-                    subtitle='Last week'
-                    avatarIcon='tabler:currency-dollar'
-                  />
-                </Grid>
+          <Grid item xs={12} lg={4}>
+            <Grid container spacing={6}>
+              <Grid item xs={6} md={3} lg={6}>
+                <CardStatsVertical
+                  stats='1.28k'
+                  chipText='-12.2%'
+                  chipColor='default'
+                  avatarColor='info'
+                  title='Net Income'
+                  subtitle='Last week'
+                  avatarIcon='tabler:currency-dollar'
+                />
+              </Grid>
 
-                <Grid item xs={6} md={3} lg={6}>
-                  <CardStatsVertical
-                    stats='1.28k'
-                    chipText='-12.2%'
-                    chipColor='default'
-                    avatarColor='error'
-                    title='Net Loss'
-                    subtitle='Last week'
-                    avatarIcon='tabler:currency-dollar'
-                  />
-                </Grid>
-                <Grid item xs={6} md={3} lg={6}>
-                  <CardStatsVertical
-                    stats='1.28k'
-                    chipText='-12.2%'
-                    chipColor='default'
-                    avatarColor='info'
-                    title='Property Value'
-                    subtitle='Last week'
-                    avatarIcon='tabler:home'
-                  />
-                </Grid>
-                <Grid item xs={6} md={3} lg={6}>
-                  <CardStatsVertical
-                    stats='1.28k'
-                    chipText='-12.2%'
-                    chipColor='default'
-                    avatarColor='warning'
-                    title='Tax Payable'
-                    subtitle='Last week'
-                    avatarIcon='tabler:receipt-2'
-                  />
-                </Grid>
+              <Grid item xs={6} md={3} lg={6}>
+                <CardStatsVertical
+                  stats='1.28k'
+                  chipText='-12.2%'
+                  chipColor='default'
+                  avatarColor='error'
+                  title='Net Loss'
+                  subtitle='Last week'
+                  avatarIcon='tabler:currency-dollar'
+                />
+              </Grid>
+              <Grid item xs={6} md={3} lg={6}>
+                <CardStatsVertical
+                  stats='1.28k'
+                  chipText='-12.2%'
+                  chipColor='default'
+                  avatarColor='info'
+                  title='Property Value'
+                  subtitle='Last week'
+                  avatarIcon='tabler:home'
+                />
+              </Grid>
+              <Grid item xs={6} md={3} lg={6}>
+                <CardStatsVertical
+                  stats='1.28k'
+                  chipText='-12.2%'
+                  chipColor='default'
+                  avatarColor='warning'
+                  title='Tax Payable'
+                  subtitle='Last week'
+                  avatarIcon='tabler:receipt-2'
+                />
               </Grid>
             </Grid>
           </Grid>
         </Grid>
-
-        <Grid item xs={12} md={12} lg={12}>
-          <AccountingTable></AccountingTable>
-        </Grid>
       </Grid>
-    </ApexChartWrapper>
+
+      <Grid item xs={12} md={12} lg={12}>
+        <AccountingTable></AccountingTable>
+      </Grid>
+    </Grid>
   )
 }
 
