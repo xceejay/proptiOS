572a02dce4c2e83a0a0ff8d3a36c4dba93fd16dd
Joel Amoako <joelkofiamoako@gmail.com>
2024-08-13 16:38:19 +0000

    Quick Auto Commit

--- src/pages/onboarding/index.js
+++ src/pages/onboarding/index.js
@@ -158,33 +158,34 @@ const Register = () => {
           }}
         >
           <Box sx={{ width: '100%', maxWidth: 400 }}>
-            <svg width={34} height={23.375} viewBox='0 0 32 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
-              <path
-                fillRule='evenodd'
-                clipRule='evenodd'
-                fill={theme.palette.primary.main}
-                d='M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z'
-              />
-              <path
-                fill='#161616'
-                opacity={0.06}
-                fillRule='evenodd'
-                clipRule='evenodd'
-                d='M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z'
-              />
-              <path
-                fill='#161616'
-                opacity={0.06}
-                fillRule='evenodd'
-                clipRule='evenodd'
-                d='M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z'
-              />
-              <path
-                fillRule='evenodd'
-                clipRule='evenodd'
-                fill={theme.palette.primary.main}
-                d='M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z'
-              />
+            <svg height={56.375} viewBox='0 0 25 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+              <g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'></g>
+              <g id='SVGRepo_iconCarrier'>
+                {' '}
+                <path
+                  d='M8 18H5.55556C3.59188 18 2 16.4081 2 14.4444V12C2 10.8954 2.89543 10 4 10C5.10457 10 6 10.8954 6 12V13.2C6 13.6418 6.35817 14 6.8 14H17.2C17.6418 14 18 13.6418 18 13.2V12C18 10.8954 18.8954 10 20 10C21.1046 10 22 10.8954 22 12V14.4444C22 16.4081 20.4081 18 18.4444 18H12'
+                  stroke={theme.palette.primary.main}
+                  stroke-width='1.5'
+                  stroke-linecap='round'
+                ></path>{' '}
+                <path
+                  d='M20 10C20 9.07069 20 8.60603 19.9231 8.21964C19.6075 6.63288 18.3671 5.39249 16.7804 5.07686C16.394 5 15.9293 5 15 5H9C8.07069 5 7.60603 5 7.21964 5.07686C5.63288 5.39249 4.39249 6.63288 4.07686 8.21964C4 8.60603 4 9.07069 4 10'
+                  stroke={theme.palette.primary.main}
+                  fill={theme.palette.primary.main}
+                  stroke-width='1.5'
+                ></path>{' '}
+                <path
+                  d='M12.75 11C12.75 10.5858 12.4142 10.25 12 10.25C11.5858 10.25 11.25 10.5858 11.25 11H12.75ZM11.25 7C11.25 7.41421 11.5858 7.75 12 7.75C12.4142 7.75 12.75 7.41421 12.75 7H11.25ZM11.25 11V14H12.75V11H11.25ZM11.25 5V7H12.75V5H11.25Z'
+                  fill={theme.palette.primary.main}
+                ></path>{' '}
+                <path
+                  d='M20 19V18M4 19V18'
+                  stroke={theme.palette.primary.main}
+                  stroke-width='1.5'
+                  stroke-linecap='round'
+                ></path>{' '}
+              </g>
             </svg>
             <Box sx={{ my: 6 }}>
               <Typography sx={{ mb: 1.5, fontWeight: 500, fontSize: '1.625rem', lineHeight: 1.385 }}>
