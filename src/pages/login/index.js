13a175278c96936c6211c498c385dac92cfdebfc
Joel Amoako <joelkofiamoako@gmail.com>
2024-08-13 16:30:58 +0000

    Quick Auto Commit

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -91,8 +91,8 @@ const schema = yup.object().shape({
 })
 
 const defaultValues = {
-  password: 'password789',
-  email: 'alicejohnson@example.com'
+  // password: 'password789',
+  // email: 'alicejohnson@example.com'
 }
 
 const LoginPage = () => {
