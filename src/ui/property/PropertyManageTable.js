9a729deb63cd0d53fddb5b49df54106c5a77e4f4
Joel Amoako <joelkofiamoako@gmail.com>
2024-08-13 15:32:06 +0000

    Quick Auto Commit

--- src/ui/property/PropertyManageTable.js
+++ src/ui/property/PropertyManageTable.js
@@ -418,15 +418,31 @@ const PropertyManageTable = ({ apiData }) => {
   )
 }
 
-export const getStaticProps = async () => {
-  const res = await axios.get('http://api.pm.manages.homes/properties')
-  const apiData = res.data
+export const getServerSideProps = async () => {
+  axios
+    .get('http://api.pm.manages.homes', {
+      params: {
+        id: 12345
+      }
+    })
+    .then(function (response) {
+      console.log('tenant table:', response.data)
 
-  return {
-    props: {
-      apiData
-    }
-  }
+      let apiData = response.data
+
+      return {
+        props: {
+          apiData
+        }
+      }
+    })
+    .catch(function (error) {
+      console.log('tenant table err:', error)
+    })
+
+  // .finally(function () {
+  //   // always executed
+  // })
 }
 
 export default PropertyManageTable
