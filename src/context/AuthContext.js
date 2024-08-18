64fe325018474d0c8f597db5d26e646fafb46bef
Joel Amoako <joelkofiamoako@gmail.com>
2024-08-18 22:42:55 +0000

    Quick Auto Commit

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -97,7 +97,6 @@ const AuthProvider = ({ children }) => {
     window.localStorage.removeItem('userData')
     window.localStorage.removeItem(authConfig.storageTokenKeyName)
     router.push('/login')
-    router.refresh()
   }
 
   const values = {
