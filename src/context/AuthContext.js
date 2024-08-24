1142b789e0604ca2114be2722b907b6b09f8316d
Joel Amoako <joelkofiamoako@gmail.com>
2024-08-24 16:18:16 +0000

    Quick Auto Commit

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -38,6 +38,10 @@ const AuthProvider = ({ children }) => {
 
         setLoading(false)
         console.log('decoded-data', decoded)
+
+        if (!decoded.payload) {
+          handleLogout()
+        }
         setUser(decoded.payload)
         setLoading(false)
 
