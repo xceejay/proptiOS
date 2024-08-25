d927ba6292dd1bcd9eadbd4712902d5c2d4a4dcc
Joel Amoako <joelkofiamoako@gmail.com>
2024-08-25 20:55:22 +0000

    Quick Auto Commit

--- src/ui/auth/RegisterFileUploader.js
+++ src/ui/auth/RegisterFileUploader.js
@@ -1,16 +1,28 @@
 import { MuiFileInput } from 'mui-file-input'
+import { useState } from 'react'
 
 const RegisterFileUploader = ({ field, file, setFile, fieldState }) => {
+  const [errorText, setErrorText] = useState('')
+
   const handleChange = newFile => {
+    const fileSizeLimit = 20 * 1024 * 1024 // 5MB limit
+
+    if (newFile && newFile.size > fileSizeLimit) {
+      setErrorText('File size exceeds 20MB')
+      setFile(null) // or handle it as needed
+    } else {
+      setErrorText('')
+      setFile(newFile)
+    }
+
     console.log('new file', newFile)
-    setFile(newFile)
   }
 
   return (
     <MuiFileInput
       {...field}
-      helperText={fieldState.invalid ? 'File is invalid' : ''}
-      error={fieldState.invalid}
+      helperText={errorText || (fieldState.invalid ? 'File is invalid' : '')}
+      error={!!errorText || fieldState.invalid}
       value={file}
       onChange={handleChange}
     />
