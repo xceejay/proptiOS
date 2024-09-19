29e2bfd49bb357464dd40ee8eafeec18c9a3aea8
Joel Amoako <joelkofiamoako@gmail.com>
2024-09-19 09:27:35 +0000

    Quick Auto Commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,29 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: globals.browser
+    },
+    plugins: {
+      react: pluginReact
+    },
+    rules: {
+      // Remove unused imports
+      'no-unused-vars': [
+        'error',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', ignoreRestSiblings: true }
+      ]
+    },
+    settings: {
+      react: {
+        version: 'detect' // Automatically detects the React version
+      }
+    }
+  },
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+  pluginReact.configs.flat.recommended
+]
