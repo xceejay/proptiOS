4e46a5f01c5ecb98e3876973540d1e2c2762d0b5
Joel Amoako <joelkofiamoako@gmail.com>
2024-09-21 12:38:03 +0000

    Quick Auto Commit

--- src/pages/leases/create/index.js
+++ src/pages/leases/create/index.js
@@ -8,12 +8,15 @@ import Grid from '@mui/material/Grid'
 import axios from 'axios'
 
 // ** Demo Components Imports
-import AddCard from 'src/views/apps/invoice/add/AddCard'
-import AddActions from 'src/views/apps/invoice/add/AddActions'
-import AddNewCustomers from 'src/views/apps/invoice/add/AddNewCustomer'
 
 // ** Styled Component
 import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
+import { Button } from '@mui/material'
+import Icon from 'src/@core/components/icon'
+
+import AddCard from 'src/views/apps/lease/add/AddCard'
+import AddActions from 'src/views/apps/lease/add/AddActions'
+import AddNewCustomer from 'src/views/apps/lease/add/AddNewCustomer'
 
 const InvoiceAdd = ({}) => {
   // ** State
@@ -23,29 +26,40 @@ const InvoiceAdd = ({}) => {
   const toggleAddCustomerDrawer = () => setAddCustomerOpen(!addCustomerOpen)
 
   return (
-    <DatePickerWrapper sx={{ '& .react-datepicker-wrapper': { width: 'auto' } }}>
-      <Grid container spacing={6}>
-        <Grid item xl={9} md={8} xs={12}>
-          <AddCard
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Grid pb={5}>
+          <Button size='small' variant='outlined' onClick={() => router.push('/tenants')}>
+            <Icon icon='tabler:arrow-left' fontSize={20} />
+            Back
+          </Button>
+        </Grid>
+
+        <DatePickerWrapper sx={{ '& .react-datepicker-wrapper': { width: 'auto' } }}>
+          <Grid container spacing={6}>
+            <Grid item xl={9} md={8} xs={12}>
+              <AddCard
+                clients={clients}
+                invoiceNumber={1}
+                selectedClient={selectedClient}
+                setSelectedClient={setSelectedClient}
+                toggleAddCustomerDrawer={toggleAddCustomerDrawer}
+              />
+            </Grid>
+            <Grid item xl={3} md={4} xs={12}>
+              <AddActions />
+            </Grid>
+          </Grid>
+          <AddNewCustomer
             clients={clients}
-            invoiceNumber={1}
-            selectedClient={selectedClient}
+            open={addCustomerOpen}
+            setClients={setClients}
+            toggle={toggleAddCustomerDrawer}
             setSelectedClient={setSelectedClient}
-            toggleAddCustomerDrawer={toggleAddCustomerDrawer}
           />
-        </Grid>
-        <Grid item xl={3} md={4} xs={12}>
-          <AddActions />
-        </Grid>
+        </DatePickerWrapper>
       </Grid>
-      <AddNewCustomers
-        clients={clients}
-        open={addCustomerOpen}
-        setClients={setClients}
-        toggle={toggleAddCustomerDrawer}
-        setSelectedClient={setSelectedClient}
-      />
-    </DatePickerWrapper>
+    </Grid>
   )
 }
 
