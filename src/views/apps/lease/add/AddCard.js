619686421b04a644a59f3408105d22d640c3b4ba
Joel Amoako <joelkofiamoako@gmail.com>
2024-09-22 12:19:45 +0000

    Quick Auto Commit

--- src/views/apps/lease/add/AddCard.js
+++ src/views/apps/lease/add/AddCard.js
@@ -394,54 +394,104 @@ const AddCard = props => {
     <Card>
       <CardContent sx={{ p: [`${theme.spacing(6)} !important`, `${theme.spacing(10)} !important`] }}>
         <Grid container>
-          <Grid item xl={6} xs={12} sx={{ mb: { xl: 0, xs: 4 } }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-              <Box sx={{ mb: 6, display: 'flex', alignItems: 'center' }}>
-                <svg width={34} height={23.375} viewBox='0 0 32 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                  <path
-                    fillRule='evenodd'
-                    clipRule='evenodd'
-                    fill={theme.palette.primary.main}
-                    d='M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z'
-                  />
-                  <path
-                    fill='#161616'
-                    opacity={0.06}
-                    fillRule='evenodd'
-                    clipRule='evenodd'
-                    d='M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z'
-                  />
-                  <path
-                    fill='#161616'
-                    opacity={0.06}
-                    fillRule='evenodd'
-                    clipRule='evenodd'
-                    d='M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z'
-                  />
-                  <path
-                    fillRule='evenodd'
-                    clipRule='evenodd'
-                    fill={theme.palette.primary.main}
-                    d='M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z'
-                  />
-                </svg>
-                <Typography
-                  variant='h6'
-                  sx={{
-                    ml: 2.5,
-                    fontWeight: 600,
-                    lineHeight: '24px',
-                    fontSize: '1.375rem !important'
-                  }}
-                >
-                  {siteId}
+          <Grid item xl={6} xs={12}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: { xl: 'flex-start', xs: 'flex-start' } }}>
+              <Box sx={{ mb: 3, display: 'flex', alignItems: 'center' }}>
+                <Typography variant='h6' sx={{ mr: 2, width: '105px' }}>
+                  Lease title
                 </Typography>
+                {/* <TextField
+                  size='small'
+                  value={invoiceNumber}
+                  sx={{ width: { sm: '250px', xs: '170px' } }}
+                  InputProps={{
+                    disabled: true,
+                    startAdornment: <InputAdornment position='start'>#</InputAdornment>
+                  }}
+                /> */}
+
+                <TextField
+                  size='small'
+                  sx={{ width: { sm: '250px', xs: '170px' } }}
+                  defaultValue='Embassy Heights 1 Bedroom Lease Agreement'
+                  value={leaseTitle}
+                  onChange={event => setLeaseTitle(event.target.value)}
+                />
               </Box>
-              <div>
-                <Typography sx={{ mb: 2, color: 'text.secondary' }}>Office 149, 450 South Brand Brooklyn</Typography>
-                <Typography sx={{ mb: 2, color: 'text.secondary' }}>San Diego County, CA 91905, USA</Typography>
-                <Typography sx={{ color: 'text.secondary' }}>+1 (123) 456 7891, +44 (876) 543 2198</Typography>
-              </div>
+              <Box sx={{ mb: 3, display: 'flex', alignItems: 'center' }}>
+                <Typography sx={{ mr: 3, width: '100px', color: 'text.secondary' }}>Date Issued:</Typography>
+                <DatePicker
+                  id='issue-date'
+                  selected={issueDate}
+                  customInput={<CustomInput />}
+                  onChange={date => setIssueDate(date)}
+                />
+              </Box>
+              <Box sx={{ mb: 3, display: 'flex', alignItems: 'center' }}>
+                <Typography sx={{ mr: 3, width: '100px', color: 'text.secondary' }}>Date Due:</Typography>
+                <DatePicker
+                  id='due-date'
+                  selected={dueDate}
+                  customInput={<CustomInput />}
+                  onChange={date => setDueDate(date)}
+                />
+              </Box>
+
+              <Box sx={{ mb: 3, display: 'flex', alignItems: 'center' }}>
+                <Typography sx={{ mr: 3, width: '100px', color: 'text.secondary' }}>Property:</Typography>
+                <FormControl>
+                  <Controller
+                    render={({ onChange, ...props }) => (
+                      <Autocomplete
+                        size='small'
+                        value={tenant}
+                        onChange={(event, newValue) => {
+                          setTenant(newValue)
+                        }}
+                        options={allTenantsData ? allTenantsData : []}
+                        getOptionLabel={tenant => tenant.name + ','} // Display the tenant name
+                        // getOptionDisabled={tenant => !!tenant.property?.id}
+                        sx={{ minWidth: 150 }}
+                        renderInput={params => (
+                          <TextField sx={{ width: { sm: '250px', xs: '170px' } }} {...params} label='Select Property' />
+                        )}
+                      />
+                    )}
+                    onChange={([, data]) => data}
+                    defaultValue={''}
+                    name={name}
+                    control={control}
+                  />
+                </FormControl>
+              </Box>
+
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Typography sx={{ mr: 3, width: '100px', color: 'text.secondary' }}>Unit:</Typography>
+                <FormControl>
+                  <Controller
+                    render={({ onChange, ...props }) => (
+                      <Autocomplete
+                        size='small'
+                        value={tenant}
+                        onChange={(event, newValue) => {
+                          setTenant(newValue)
+                        }}
+                        options={allTenantsData ? allTenantsData : []}
+                        getOptionLabel={tenant => tenant.name + ','} // Display the tenant name
+                        // getOptionDisabled={tenant => !!tenant.property?.id}
+                        sx={{ minWidth: 150 }}
+                        renderInput={params => (
+                          <TextField sx={{ width: { sm: '250px', xs: '170px' } }} {...params} label='Select Unit' />
+                        )}
+                      />
+                    )}
+                    onChange={([, data]) => data}
+                    defaultValue={''}
+                    name={name}
+                    control={control}
+                  />
+                </FormControl>
+              </Box>
             </Box>
           </Grid>
           <Grid item xl={6} xs={12}>
@@ -477,7 +527,7 @@ const AddCard = props => {
                   onChange={date => setIssueDate(date)}
                 />
               </Box>
-              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Box sx={{ mb: 3, display: 'flex', alignItems: 'center' }}>
                 <Typography sx={{ mr: 3, width: '100px', color: 'text.secondary' }}>Date Due:</Typography>
                 <DatePicker
                   id='due-date'
@@ -486,12 +536,123 @@ const AddCard = props => {
                   onChange={date => setDueDate(date)}
                 />
               </Box>
+
+              <Box sx={{ mb: 3, display: 'flex', alignItems: 'center' }}>
+                <Typography sx={{ mr: 3, width: '100px', color: 'text.secondary' }}>Property:</Typography>
+                <FormControl>
+                  <Controller
+                    render={({ onChange, ...props }) => (
+                      <Autocomplete
+                        size='small'
+                        value={tenant}
+                        onChange={(event, newValue) => {
+                          setTenant(newValue)
+                        }}
+                        options={allTenantsData ? allTenantsData : []}
+                        getOptionLabel={tenant => tenant.name + ','} // Display the tenant name
+                        // getOptionDisabled={tenant => !!tenant.property?.id}
+                        sx={{ minWidth: 150 }}
+                        renderInput={params => (
+                          <TextField sx={{ width: { sm: '250px', xs: '170px' } }} {...params} label='Select Property' />
+                        )}
+                      />
+                    )}
+                    onChange={([, data]) => data}
+                    defaultValue={''}
+                    name={name}
+                    control={control}
+                  />
+                </FormControl>
+              </Box>
+
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Typography sx={{ mr: 3, width: '100px', color: 'text.secondary' }}>Unit:</Typography>
+                <FormControl>
+                  <Controller
+                    render={({ onChange, ...props }) => (
+                      <Autocomplete
+                        size='small'
+                        value={tenant}
+                        onChange={(event, newValue) => {
+                          setTenant(newValue)
+                        }}
+                        options={allTenantsData ? allTenantsData : []}
+                        getOptionLabel={tenant => tenant.name + ','} // Display the tenant name
+                        // getOptionDisabled={tenant => !!tenant.property?.id}
+                        sx={{ minWidth: 150 }}
+                        renderInput={params => (
+                          <TextField sx={{ width: { sm: '250px', xs: '170px' } }} {...params} label='Select Unit' />
+                        )}
+                      />
+                    )}
+                    onChange={([, data]) => data}
+                    defaultValue={''}
+                    name={name}
+                    control={control}
+                  />
+                </FormControl>
+              </Box>
             </Box>
           </Grid>
         </Grid>
 
+        <Divider sx={{ mt: 10 }}></Divider>
+
         <Grid sx={{ mt: 10 }} container>
-          <Grid item xl={6} xs={12} sx={{ mb: { xl: 0, xs: 4 } }}>
+          <Grid container>
+            <Grid item xl={6} xs={12} sx={{ mb: { xl: 0, xs: 4 } }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Box sx={{ mb: 6, display: 'flex', alignItems: 'center' }}>
+                  <svg width={34} height={23.375} viewBox='0 0 32 22' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                    <path
+                      fillRule='evenodd'
+                      clipRule='evenodd'
+                      fill={theme.palette.primary.main}
+                      d='M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z'
+                    />
+                    <path
+                      fill='#161616'
+                      opacity={0.06}
+                      fillRule='evenodd'
+                      clipRule='evenodd'
+                      d='M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z'
+                    />
+                    <path
+                      fill='#161616'
+                      opacity={0.06}
+                      fillRule='evenodd'
+                      clipRule='evenodd'
+                      d='M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z'
+                    />
+                    <path
+                      fillRule='evenodd'
+                      clipRule='evenodd'
+                      fill={theme.palette.primary.main}
+                      d='M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z'
+                    />
+                  </svg>
+                  <Typography
+                    variant='h6'
+                    sx={{
+                      ml: 2.5,
+                      fontWeight: 600,
+                      lineHeight: '24px',
+                      fontSize: '1.375rem !important'
+                    }}
+                  >
+                    {siteId}
+                  </Typography>
+                </Box>
+                <div>
+                  <Typography sx={{ mb: 2, color: 'text.secondary' }}>Office 149, 450 South Brand Brooklyn</Typography>
+                  <Typography sx={{ mb: 2, color: 'text.secondary' }}>San Diego County, CA 91905, USA</Typography>
+                  <Typography sx={{ color: 'text.secondary' }}>+1 (123) 456 7891, +44 (876) 543 2198</Typography>
+                </div>
+              </Box>
+            </Grid>
+            <Grid item xl={6} xs={12}></Grid>
+          </Grid>
+          <Grid item xl={6} xs={12} sx={{ mt: 10, mb: { xl: 0, xs: 4 } }}>
             <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'left', justifyContent: 'flex-start' }}>
               <CalcWrapper flex={0.5}>
                 <Typography>Dear </Typography>
