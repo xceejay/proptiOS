dd8a02780b22121f867b868b4474b7faf6bbab2e
Joel Amoako <joelkofiamoako@gmail.com>
2024-11-01 15:47:51 +0000

    Quick Auto Commit

--- src/ui/property/ParentPropertyOverview.js
+++ src/ui/property/ParentPropertyOverview.js
@@ -1,6 +1,5 @@
-import { Grid, Typography } from '@mui/material'
-import PropertyManageTable from './PropertyManageTable'
 import CardStatsVertical from 'src/@core/components/card-statistics/card-stats-vertical'
+import { Grid, Link, Typography } from '@mui/material'
 
 const ParentPropertyViewOverview = ({ setPropertiesData, propertiesData }) => {
   return (
@@ -9,131 +8,101 @@ const ParentPropertyViewOverview = ({ setPropertiesData, propertiesData }) => {
         <Grid container spacing={6} sx={{ mb: 4 }}>
           {propertiesData.length > 0 ? (
             <>
-              <Grid
-                item
-                xs={6}
-                sm={6}
-                lg={3}
-                onClick={() => {
-                  window.location.href = '/tenants'
-                }}
-              >
-                <CardStatsVertical
-                  chipText={propertiesData.reduce((total, item) => total + item.total_tenants, 0)}
-                  avatarColor='success'
-                  chipColor='default'
-                  title='Total Tenants'
-                  subtitle='Number of tenants'
-                  avatarIcon='tabler:user'
-                />
+              <Grid item xs={6} sm={6} lg={3}>
+                <Link href='/tenants' underline='none' color='inherit'>
+                  <CardStatsVertical
+                    chipText={propertiesData.reduce((total, item) => total + item.total_tenants, 0)}
+                    avatarColor='success'
+                    chipColor='default'
+                    title='Total Tenants'
+                    subtitle='Number of tenants'
+                    avatarIcon='tabler:user'
+                  />
+                </Link>
               </Grid>
-              <Grid
-                item
-                xs={6}
-                sm={6}
-                lg={3}
-                onClick={() => {
-                  window.location.href = '/tenants'
-                }}
-              >
-                <CardStatsVertical
-                  chipText={propertiesData.reduce((total, item) => total + item.active_tenants, 0)}
-                  avatarColor='success'
-                  chipColor='default'
-                  title='Active Tenants'
-                  subtitle='Tenants with active account'
-                  avatarIcon='tabler:user-check'
-                />
+              <Grid item xs={6} sm={6} lg={3}>
+                <Link href='/tenants' underline='none' color='inherit'>
+                  <CardStatsVertical
+                    chipText={propertiesData.reduce((total, item) => total + item.active_tenants, 0)}
+                    avatarColor='success'
+                    chipColor='default'
+                    title='Active Tenants'
+                    subtitle='Tenants with active account'
+                    avatarIcon='tabler:user-check'
+                  />
+                </Link>
               </Grid>
-              <Grid
-                item
-                xs={6}
-                sm={6}
-                lg={3}
-                onClick={() => {
-                  window.location.href = '/leases'
-                }}
-              >
-                <CardStatsVertical
-                  chipText={propertiesData.reduce((total, item) => total + item.total_leases, 0)}
-                  avatarColor='warning'
-                  chipColor='default'
-                  title='Total Leases'
-                  subtitle='Leases active'
-                  avatarIcon='tabler:file'
-                />
+              <Grid item xs={6} sm={6} lg={3}>
+                <Link href='/leases' underline='none' color='inherit'>
+                  <CardStatsVertical
+                    chipText={propertiesData.reduce((total, item) => total + item.total_leases, 0)}
+                    avatarColor='warning'
+                    chipColor='default'
+                    title='Total Leases'
+                    subtitle='Leases active'
+                    avatarIcon='tabler:file'
+                  />
+                </Link>
               </Grid>
-              <Grid
-                item
-                xs={6}
-                sm={6}
-                lg={3}
-                onClick={() => {
-                  window.location.href = '/properties/management'
-                }}
-              >
-                <CardStatsVertical
-                  chipText={propertiesData.reduce((total, item) => total + item.total_maintenance_requests, 0)}
-                  avatarColor='secondary'
-                  chipColor='default'
-                  title='Maintenance Requests'
-                  subtitle='Pending requests'
-                  avatarIcon='tabler:tools'
-                />
+              <Grid item xs={6} sm={6} lg={3}>
+                <Link href='/properties/management' underline='none' color='inherit'>
+                  <CardStatsVertical
+                    chipText={propertiesData.reduce((total, item) => total + item.total_maintenance_requests, 0)}
+                    avatarColor='secondary'
+                    chipColor='default'
+                    title='Maintenance Requests'
+                    subtitle='Pending requests'
+                    avatarIcon='tabler:tools'
+                  />
+                </Link>
               </Grid>
-              <Grid
-                item
-                xs={6}
-                sm={6}
-                lg={3}
-                onClick={() => {
-                  window.location.href = '/properties/management'
-                }}
-              >
-                <CardStatsVertical
-                  chipText={propertiesData.reduce((total, item) => total + item.total_applicants, 0)}
-                  avatarColor='info'
-                  chipColor='default'
-                  title='Total Applicants'
-                  subtitle='Applicants count'
-                  avatarIcon='tabler:user-check'
-                />
+              <Grid item xs={6} sm={6} lg={3}>
+                <Link href='/properties/management' underline='none' color='inherit'>
+                  <CardStatsVertical
+                    chipText={propertiesData.reduce((total, item) => total + item.pending_maintenance_requests, 0)}
+                    avatarColor='error'
+                    chipColor='default'
+                    title='Pending Maintenance Requests'
+                    subtitle='Pending requests'
+                    avatarIcon='tabler:tools'
+                  />
+                </Link>
               </Grid>
-              <Grid
-                item
-                xs={6}
-                sm={6}
-                lg={3}
-                onClick={() => {
-                  window.location.href = '/properties/management'
-                }}
-              >
-                <CardStatsVertical
-                  chipText={propertiesData.reduce((total, item) => total + item.units_vacant, 0)}
-                  avatarColor='error'
-                  chipColor='default'
-                  title='Vacant Units'
-                  subtitle='Units not occupied'
-                  avatarIcon='tabler:door'
-                />
+              <Grid item xs={6} sm={6} lg={3}>
+                <Link href='/properties/management' underline='none' color='inherit'>
+                  <CardStatsVertical
+                    chipText={propertiesData.reduce((total, item) => total + item.total_applicants, 0)}
+                    avatarColor='info'
+                    chipColor='default'
+                    title='Total Applicants'
+                    subtitle='Applicants count'
+                    avatarIcon='tabler:user-check'
+                  />
+                </Link>
               </Grid>
-              <Grid
-                item
-                xs={6}
-                sm={6}
-                lg={3}
-                onClick={() => {
-                  window.location.href = '/leases'
-                }}
-              >
-                <CardStatsVertical
-                  chipText={propertiesData.reduce((total, item) => total + item.leases_expiring_soon, 0)}
-                  avatarColor='error'
-                  chipColor='default'
-                  title='Leases Expiring Soon'
-                  subtitle='Leases expiring in next 30 days'
-                  avatarIcon='tabler:alarm'
-                />
+              <Grid item xs={6} sm={6} lg={3}>
+                <Link href='/properties/management' underline='none' color='inherit'>
+                  <CardStatsVertical
+                    chipText={propertiesData.reduce((total, item) => total + item.units_vacant, 0)}
+                    avatarColor='error'
+                    chipColor='default'
+                    title='Vacant Units'
+                    subtitle='Units not occupied'
+                    avatarIcon='tabler:door'
+                  />
+                </Link>
+              </Grid>
+              <Grid item xs={6} sm={6} lg={3}>
+                <Link href='/leases' underline='none' color='inherit'>
+                  <CardStatsVertical
+                    chipText={propertiesData.reduce((total, item) => total + item.leases_expiring_soon, 0)}
+                    avatarColor='error'
+                    chipColor='default'
+                    title='Leases Expiring Soon'
+                    subtitle='Leases expiring in next 30 days'
+                    avatarIcon='tabler:alarm'
+                  />
+                </Link>
               </Grid>
             </>
           ) : // <Grid item xs={12} sx={{ textAlign: 'center', p: 4 }}>
