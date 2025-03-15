c37bb800a928855091152b82f12a74f98593eb7f
Joel Amoako <joelkofiamoako@gmail.com>
2025-03-15 16:33:10 +0000

    Quick Auto Commit

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -86,6 +86,7 @@ const Dashboard = () => {
                 />
               </Link>
             </Grid>
+            {/* Grid for properties */}
             <Grid item xs={12} sm={6} lg={3}>
               <Link href='/properties' style={{ textDecoration: 'none' }}>
                 <CardStatsVertical
@@ -98,6 +99,7 @@ const Dashboard = () => {
                 />
               </Link>
             </Grid>
+            {/* Grid for properties */}
             <Grid item xs={12} sm={6} lg={3}>
               <Link href='/properties' style={{ textDecoration: 'none' }}>
                 <CardStatsVertical
